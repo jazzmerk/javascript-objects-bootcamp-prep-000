bdb6160856145b226bfb27ce942344e73cd8a412
James Merkin <jnmerk@gmail.com>
2019-03-30 17:21:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist ={[artistname]:[songtitle]};
 function updatePlaylist (playlist, artistname, songtitle)  {
-Objest.assign ({},playlist,{artistname:[playlist]}); 
+Object.assign ({},playlist,{artistname:[playlist]}); 
 }
