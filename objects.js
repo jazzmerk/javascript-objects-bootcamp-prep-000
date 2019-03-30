741d3dd43e71d8f702b5279c5b27d8344148ddd9
James Merkin <jnmerk@gmail.com>
2019-03-30 17:27:51 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
-var playlist ={[artistname]:[songtitle]};
-
+var playlist =new Object{[artistname]:[songtitle]};
+playlist.artistname = "John Coltrane":
 
 function updatePlaylist (playlist, artistname, songtitle)  {
 Object.assign ({},playlist,{artistname:[playlist]}); 
